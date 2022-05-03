df6591b289819e51c68b0124038961cb86a15db7
Anna Rolfes <anna-rolfes@gmx.de>
2022-05-03 22:55:57 +0200

    Add ColorForm incl. component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import { useState } from 'react';
 import { colors } from './db';
 import StyledColorCard from './components/StyledColorCard';
-
 import StyledColorCardWrapper from './components/StyledColorCardWrapper';
 import ColorTag from './components/ColorTag';
 
+function ColorForm({ onSubmit }) {
+  const [color, setColor] = useState('#FFCDB2');
+  return (
+    <form
+      action=""
+      onSubmit={event => {
+        event.preventDefault();
+        onSubmit(color);
+      }}
+    >
+      <input type="color" value={color} onChange={handleChange}></input>
+      <input type="text" value={color} onChange={handleChange}></input>
+      <button type="submit">Add color</button>
+    </form>
+  );
+
+  function handleChange(event) {
+    setColor(event.target.value);
+  }
+}
+
 function App() {
+  const [colorList, setColorList] = useState(colors);
   return (
     <>
       <h1>Color Saver</h1>
       <StyledColorCardWrapper>
+        <ColorForm onSubmit={handleSubmit} />
         {colors.map(color => {
           return (
             <StyledColorCard backgroundColor={color}>
@@ -19,6 +42,9 @@ function App() {
       </StyledColorCardWrapper>
     </>
   );
+  function handleSubmit(newColor) {
+    setColorList([newColor, ...colorList]);
+  }
 }
 
 export default App;
