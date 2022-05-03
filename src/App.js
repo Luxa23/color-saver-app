1fa063c8630ae2d920ec1f557fa778783b6b9063
Carolin Moellercat-miller <github@carolinmoeller.com>
2022-05-03 16:08:31 +0200

    Create ColorCards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import "./App.css";
+import './App.css';
+import styled from 'styled-components';
+import { colors } from './db';
+
+const StyledColorCard = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: ${({ backgroundColor = 'hotpink' }) => backgroundColor};
+  border-radius: 15px;
+`;
 
 function App() {
   return (
     <div className="App">
-      <h1>Quizz App React</h1>
+      <h1>Color Saver</h1>
+      {colors.map(color => {
+        return <StyledColorCard backgroundColor={color}></StyledColorCard>;
+      })}
     </div>
   );
 }
---
+++ src/db.js
@@ -0,0 +1,1 @@
+export const colors = ['#FFCDB2', '#FFB4A2', '#E5989B', '#B5838D', '#6D6875'];
