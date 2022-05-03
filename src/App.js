f794b297da16ae34ce5530b5dd363892985f5c0a
Carolin Moellercat-miller <github@carolinmoeller.com>
2022-05-03 16:20:04 +0200

    Add StyledCalorTags to cards

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import styled from 'styled-components';
 import { colors } from './db';
 
 const StyledColorCard = styled.div`
-  width: 100px;
-  height: 100px;
+  width: 150px;
+  height: 150px;
   background-color: ${({ backgroundColor = 'hotpink' }) => backgroundColor};
   border-radius: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledColorTag = styled.span`
+  background-color: white;
+  padding: 0.5rem;
+  border-radius: 999px;
 `;
 
 function App() {
@@ -14,7 +23,11 @@ function App() {
     <div className="App">
       <h1>Color Saver</h1>
       {colors.map(color => {
-        return <StyledColorCard backgroundColor={color}></StyledColorCard>;
+        return (
+          <StyledColorCard backgroundColor={color}>
+            <StyledColorTag>{color}</StyledColorTag>
+          </StyledColorCard>
+        );
       })}
     </div>
   );
